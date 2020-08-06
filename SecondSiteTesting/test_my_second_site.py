4e1ce4043b39868b5753f934bfa6e58cc8f8a84d
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2020-08-06 12:40:06 +0100

    Updating README.md and commenting in automation.py

---
+++ SecondSiteTesting/test_my_second_site.py
@@ -0,0 +1,21 @@
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+
+# Starter essential code to get Selenium for Python running.
+
+from selenium import webdriver          
+
+chrome_browser = webdriver.Chrome('./chromedriver')
+
+print(chrome_browser) 
+# Output in the terminal is <selenium.webdriver.chrome.webdriver.WebDriver (session=" A BUNCH OF NUMBERS HERE ")>
+
+# ###############################################################################
+# ###############################################################################
+# ###############################################################################
+
+chrome_browser.maximize_window()
+# What do you want Selenium to test?
+chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html');
+
