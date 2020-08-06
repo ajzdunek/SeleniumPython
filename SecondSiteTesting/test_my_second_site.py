de14036f4756b8a1e9ad7a6d73afa16e7aac0f02
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2020-08-06 12:57:53 +0100

    Updating README.md for SecondSiteTesting

--- SecondSiteTesting/test_my_second_site.py
+++ SecondSiteTesting/test_my_second_site.py
@@ -8,8 +8,8 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-print(chrome_browser) 
-# Output in the terminal is <selenium.webdriver.chrome.webdriver.WebDriver (session=" A BUNCH OF NUMBERS HERE ")>
+# print(chrome_browser) 
+# Output for print(chrome_browser) in the terminal is <selenium.webdriver.chrome.webdriver.WebDriver (session=" A BUNCH OF NUMBERS HERE ")>
 
 # ###############################################################################
 # ###############################################################################
@@ -18,4 +18,6 @@ print(chrome_browser)
 chrome_browser.maximize_window()
 # What do you want Selenium to test?
 chrome_browser.get('https://www.andrewzdunek.com/');
+
+
 
